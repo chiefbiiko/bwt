cf76d13bb78ef97877cc365dbad33e1adfbc4153
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-04-14 17:44:29 +0200

    catchin em all

--- mod.ts
+++ mod.ts
@@ -1,15 +1,12 @@
-import {
-  seal as aeadChaCha20Poly1305Seal,
-  open as aeadChaCha20Poly1305Open,
-  NONCE_BYTES,
-  TAG_BYTES
-} from "https://denopkg.com/chiefbiiko/aead-chacha20-poly1305/mod.ts";
-import { constantTimeEqual } from "https://denopkg.com/chiefbiiko/aead-chacha20-poly1305/constant_time_equal/constant_time_equal.ts";
 import { Curve25519 } from "https://denopkg.com/chiefbiiko/curve25519/mod.ts";
 import {
   toUint8Array as base64ToUint8Array,
   fromUint8Array as base64FromUint8Array
 } from "https://denopkg.com/chiefbiiko/base64/mod.ts";
+import {
+  seal as aeadChaCha20Poly1305Seal,
+  open as aeadChaCha20Poly1305Open
+} from "https://denopkg.com/chiefbiiko/aead-chacha20-poly1305/mod.ts";
 
 export interface Metadata {
   typ: string;
@@ -18,10 +15,6 @@ export interface Metadata {
   kid: string;
   iat: number;
   exp: number;
-}
-
-interface InternalMetadata extends Metadata {
-  nonce: Uint8Array;
 }
 
 export interface Payload {
@@ -53,17 +46,17 @@ function nextNonce(): Uint8Array {
 }
 
 // TODO:
-//   + catch all JSON* and base64* errors
 //   + think about code usage patterns & whether caching the key in the factory
 //     makes sense
 //       -> createStringify(/**/), stringify(metadata, payload)
 //       -> createParse(/**/), parse(token)
 //   + make standalone stringify and parse
 //   + implement bwt key set feature: Set [{kid:"abc",pk:"xyz"},{/**/}]
+//   + implement a better default nonce gen func
 //   + revisit and polish all dependencies
 
 function isValidMetadata(
-  metadata: Metadata,
+  metadata: { [key: string]: any },
   checkExpiry: boolean = true
 ): boolean {
   return (
@@ -101,66 +94,58 @@ export function createAuthenticator({
       if (!payload || !isValidMetadata(metadata, false)) {
         return null;
       }
-      const nonce: Uint8Array = nextNonce();
-      if (nonce.length !== NONCE_BYTES) {
-        return null;
-      }
+      let nonce: Uint8Array;
+      let metadataPlusNonce: { [key: string]: any };
       let aad: Uint8Array;
       let plaintext: Uint8Array;
       let aead: { ciphertext: Uint8Array; tag: Uint8Array };
+      let head: string, body: string, tail: string;
       try {
-        aad = enc.encode(
-          JSON.stringify(
-            Object.assign({}, metadata, {
-              nonce: Array.from(nonce)
-            })
-          )
-        );
+        nonce = nextNonce();
+        metadataPlusNonce = Object.assign({}, metadata, {
+          nonce: Array.from(nonce)
+        });
+        aad = enc.encode(JSON.stringify(metadataPlusNonce));
         plaintext = enc.encode(JSON.stringify(payload));
         aead = aeadChaCha20Poly1305Seal(key, nonce, plaintext, aad);
+        head = base64FromUint8Array(aad);
+        body = base64FromUint8Array(aead.ciphertext);
+        tail = base64FromUint8Array(aead.tag);
+        return `${head}.${body}.${tail}`;
       } catch (_) {
         return null;
       }
-      return (
-        base64FromUint8Array(aad) +
-        "." +
-        base64FromUint8Array(aead.ciphertext) +
-        "." +
-        base64FromUint8Array(aead.tag)
-      );
     },
     parse(token: string): { metadata: Metadata; payload: Payload } {
-      if (!token) {
-        return null;
-      }
-      const parts: string[] = token.split(".");
-      if (parts.length !== 3) {
-        return null;
-      }
-      const aad: Uint8Array = base64ToUint8Array(parts[0]);
-      const metadata: InternalMetadata = JSON.parse(dec.decode(aad));
-      const ciphertext: Uint8Array = base64ToUint8Array(parts[1]);
-      const tag: Uint8Array = base64ToUint8Array(parts[2]);
-      const plaintext: Uint8Array = aeadChaCha20Poly1305Open(
-        key,
-        Uint8Array.from(metadata.nonce),
-        ciphertext,
-        aad,
-        tag
-      );
-      if (!plaintext) {
-        return null;
-      }
+      let parts: string[];
+      let aad: Uint8Array;
+      let metadataPlusNonce: { [key: string]: any };
+      let ciphertext: Uint8Array;
+      let tag: Uint8Array;
+      let plaintext: Uint8Array;
       let payload: Payload;
       try {
+        parts = token.split(".");
+        aad = base64ToUint8Array(parts[0]);
+        metadataPlusNonce = JSON.parse(dec.decode(aad));
+        ciphertext = base64ToUint8Array(parts[1]);
+        tag = base64ToUint8Array(parts[2]);
+        plaintext = aeadChaCha20Poly1305Open(
+          key,
+          Uint8Array.from(metadataPlusNonce.nonce),
+          ciphertext,
+          aad,
+          tag
+        );
         payload = JSON.parse(dec.decode(plaintext));
       } catch (_) {
         return null;
       }
-      if (!payload || !isValidMetadata(metadata, true)) {
+      if (!payload || !isValidMetadata(metadataPlusNonce, true)) {
         return null;
       }
-      return { metadata, payload };
+      metadataPlusNonce.nonce = undefined;
+      return { metadata: metadataPlusNonce as Metadata, payload };
     }
   };
 }
